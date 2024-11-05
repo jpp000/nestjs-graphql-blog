a0cb5f1d07e5b9efae2de0d8e6ad9d472db08ad4
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-05 20:59:20 -0300

    register usecases

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -5,6 +5,10 @@ import { PrismaService } from '@/database/prisma/prisma.service'
 import { AuthorsPrismaRepository } from './repositories/authors-prisma.repository'
 import { ListAuthorsUseCase } from './usecases/list-authors.usecase'
 import { IAuthorsRepository } from './interfaces/authors.repository'
+import { GetAuthorUseCase } from './usecases/get-author.usecase'
+import { CreateAuthorUseCase } from './usecases/create-author.usecase'
+import { DeleteAuthorUseCase } from './usecases/delete-author.usecase'
+import { UpdateAuthorUseCase } from './usecases/update-author.usecase'
 
 @Module({
   imports: [DatabaseModule],
@@ -20,6 +24,30 @@ import { IAuthorsRepository } from './interfaces/authors.repository'
       provide: ListAuthorsUseCase.UseCase,
       useFactory: (authorsRepository: IAuthorsRepository) =>
         new ListAuthorsUseCase.UseCase(authorsRepository),
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: GetAuthorUseCase.UseCase,
+      useFactory: (authorsRepository: IAuthorsRepository) =>
+        new GetAuthorUseCase.UseCase(authorsRepository),
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: CreateAuthorUseCase.UseCase,
+      useFactory: (authorsRepository: IAuthorsRepository) =>
+        new CreateAuthorUseCase.UseCase(authorsRepository),
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: DeleteAuthorUseCase.UseCase,
+      useFactory: (authorsRepository: IAuthorsRepository) =>
+        new DeleteAuthorUseCase.UseCase(authorsRepository),
+      inject: ['AuthorsRepository'],
+    },
+    {
+      provide: UpdateAuthorUseCase.UseCase,
+      useFactory: (authorsRepository: IAuthorsRepository) =>
+        new UpdateAuthorUseCase.UseCase(authorsRepository),
       inject: ['AuthorsRepository'],
     },
   ],
--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -4,11 +4,27 @@ import { Inject } from '@nestjs/common'
 import { ListAuthorsUseCase } from '@/authors/usecases/list-authors.usecase'
 import { SearchParamsArgs } from '../args/search-params.args'
 import { SearchAuthorsResult } from '../models/search-authors-result'
+import { GetAuthorUseCase } from '@/authors/usecases/get-author.usecase'
+import { CreateAuthorUseCase } from '@/authors/usecases/create-author.usecase'
+import { DeleteAuthorUseCase } from '@/authors/usecases/delete-author.usecase'
+import { UpdateAuthorUseCase } from '@/authors/usecases/update-author.usecase'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
   @Inject(ListAuthorsUseCase.UseCase)
   private readonly listAuthorsUseCase: ListAuthorsUseCase.UseCase
+
+  @Inject(GetAuthorUseCase.UseCase)
+  private readonly getAuthorUseCase: GetAuthorUseCase.UseCase
+
+  @Inject(CreateAuthorUseCase.UseCase)
+  private readonly createAuthorUseCase: CreateAuthorUseCase.UseCase
+
+  @Inject(DeleteAuthorUseCase.UseCase)
+  private readonly deleteAuthorUseCase: DeleteAuthorUseCase.UseCase
+
+  @Inject(UpdateAuthorUseCase.UseCase)
+  private readonly updateAuthorUseCase: UpdateAuthorUseCase.UseCase
 
   @Query(() => SearchAuthorsResult)
   authors(@Args() searchParamsArgs: SearchParamsArgs) {
