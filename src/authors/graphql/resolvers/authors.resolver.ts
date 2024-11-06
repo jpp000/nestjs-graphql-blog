024272ebd407a50b95a0eb2ac6db5d215de9a7a8
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-05 22:00:27 -0300

    authors resolver

--- src/authors/graphql/resolvers/authors.resolver.ts
+++ src/authors/graphql/resolvers/authors.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { Author } from '../models/author'
 import { Inject } from '@nestjs/common'
 import { ListAuthorsUseCase } from '@/authors/usecases/list-authors.usecase'
@@ -8,6 +8,9 @@ import { GetAuthorUseCase } from '@/authors/usecases/get-author.usecase'
 import { CreateAuthorUseCase } from '@/authors/usecases/create-author.usecase'
 import { DeleteAuthorUseCase } from '@/authors/usecases/delete-author.usecase'
 import { UpdateAuthorUseCase } from '@/authors/usecases/update-author.usecase'
+import { CreateAuthorInput } from '../inputs/create-author.input'
+import { AuthorIdArgs } from '../args/author-id.args'
+import { UpdateAuthorInput } from '../inputs/update-author.input'
 
 @Resolver(() => Author)
 export class AuthorsResolver {
@@ -20,14 +23,37 @@ export class AuthorsResolver {
   @Inject(CreateAuthorUseCase.UseCase)
   private readonly createAuthorUseCase: CreateAuthorUseCase.UseCase
 
-  @Inject(DeleteAuthorUseCase.UseCase)
-  private readonly deleteAuthorUseCase: DeleteAuthorUseCase.UseCase
-
   @Inject(UpdateAuthorUseCase.UseCase)
   private readonly updateAuthorUseCase: UpdateAuthorUseCase.UseCase
+
+  @Inject(DeleteAuthorUseCase.UseCase)
+  private readonly deleteAuthorUseCase: DeleteAuthorUseCase.UseCase
 
   @Query(() => SearchAuthorsResult)
   authors(@Args() searchParamsArgs: SearchParamsArgs) {
     return this.listAuthorsUseCase.execute(searchParamsArgs)
+  }
+
+  @Mutation(() => Author)
+  createAuthor(@Args('data') data: CreateAuthorInput) {
+    return this.createAuthorUseCase.execute(data)
+  }
+
+  @Query(() => Author)
+  getAuthorById(@Args() input: AuthorIdArgs) {
+    return this.getAuthorUseCase.execute(input)
+  }
+
+  @Mutation(() => Author)
+  updateAuthor(
+    @Args() { id }: AuthorIdArgs,
+    @Args('data') data: UpdateAuthorInput,
+  ) {
+    return this.updateAuthorUseCase.execute({ id, ...data })
+  }
+
+  @Mutation(() => Author)
+  deleteAuthor(@Args() input: AuthorIdArgs) {
+    return this.deleteAuthorUseCase.execute(input)
   }
 }
