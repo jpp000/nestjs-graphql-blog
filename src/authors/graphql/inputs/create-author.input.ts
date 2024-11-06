1d6a3cbc90d89e06f3fa6d66855d5f6a1a03a30a
jpp000 <141282511+jpp000@users.noreply.github.com>
2024-11-06 09:22:44 -0300

    class validator

--- src/authors/graphql/inputs/create-author.input.ts
+++ src/authors/graphql/inputs/create-author.input.ts
@@ -1,10 +1,15 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 @InputType()
 export class CreateAuthorInput {
+  @IsString()
+  @IsNotEmpty()
   @Field()
   name: string
 
+  @IsEmail()
+  @IsNotEmpty()
   @Field()
   email: string
 }
